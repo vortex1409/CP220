0d036b2e6c86d80b9883d7dec473b4851c6ad44b
Michael <mvd1409@gmail.com>
2017-04-06 11:35:39 -0400

    Create W10.cs

---
+++ W10.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W10___MDORFMAN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          // Do Stuff
+        }
+    }
+}
